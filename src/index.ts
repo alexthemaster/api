76c31c478299e738fe4179440fb1f36867583502
QuantumlyTangled <ethoarx@gmail.com>
2020-07-20 20:43:21 +0200

    feat(core/api/http): add base response and request forms

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export * from './lib/SkyraAPI';
+export * from './lib/structures/api/ApiRequest';
+export * from './lib/structures/api/ApiResponse';
+export * from './lib/structures/api/CookieStore';
+export * from './lib/structures/http/Server';
+export * from './lib/utils/Mime';
 
 export const main = () => 'this builds and pushes';
---
+++ src/lib/structures/api/ApiResponse.ts
@@ -0,0 +1,58 @@
+import { ServerResponse, STATUS_CODES } from 'http';
+import { Types as MimeTypes } from '../../utils/Mime';
+import type { CookieStore } from './CookieStore';
+
+export class ApiResponse extends ServerResponse {
+	public cookies!: CookieStore;
+
+	public ok(data: unknown = STATUS_CODES[200]) {
+		this.status(200);
+		return typeof data === 'string' ? this.text(data) : this.json(data);
+	}
+
+	public noContent() {
+		return this.status(204).end();
+	}
+
+	public badRequest(data: unknown = STATUS_CODES[400]) {
+		return this.status(400).json({ reason: data });
+	}
+
+	public unauthorized(data: unknown = STATUS_CODES[401]) {
+		return this.status(401).json({ reason: data });
+	}
+
+	public forbidden(data: unknown = STATUS_CODES[403]) {
+		return this.status(403).json({ reason: data });
+	}
+
+	public notFound(data: unknown = STATUS_CODES[404]) {
+		return this.status(404).json({ reason: data });
+	}
+
+	public error(error: number | string): void {
+		if (typeof error === 'string') {
+			return this.status(500).json({ error });
+		}
+
+		return this.status(error).json({ error: STATUS_CODES[error] });
+	}
+
+	public status(code: number): this {
+		this.statusCode = code;
+		return this;
+	}
+
+	public json(data: any): void {
+		this.setContentType(MimeTypes.ApplicationJson).end(JSON.stringify(data));
+	}
+
+	public text(data: string): void {
+		this.setContentType(MimeTypes.TextPlain).end(data);
+	}
+
+	public setContentType(contentType: MimeTypes) {
+		this.setHeader('Content-Type', contentType);
+		return this;
+	}
+}
