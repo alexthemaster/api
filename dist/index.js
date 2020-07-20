23f98d1c09081e71b7948504c8d3f7739af86fe9
Favna <contact@skyra.pw>
2020-07-20 11:53:45 +0000

    build: tsc build for 9e12ab0c0098e6e06304305901e61cb8afbbd2af

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = void 0;
+exports.main = () => 'this builds and pushes';
+exports.default = exports.main;
+//# sourceMappingURL=index.js.map
